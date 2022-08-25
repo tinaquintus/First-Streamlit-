4c51fcec7e910204b3b35e86a8edddc95ed1daae
tinaquintus <62328069+tinaquintus@users.noreply.github.com>
2022-08-25 14:52:46 +0530

    Add files via upload

---
+++ First_Streamlit.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Hello Streamlit')
+st.subheader('Welcome to my first streamlit app.')
+st.subheader('Streamlit is an open source app framework in Python language. It helps us create web apps for data science and machine learning in a short time. It is compatible with major Python libraries such as scikit-learn, Keras, PyTorch, SymPy(latex), NumPy, pandas, Matplotlib etc.')
